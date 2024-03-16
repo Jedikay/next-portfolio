66a0d550d04905675955f37adc95b50e5df6e296
Jedikay <kadyns0604@gmail.com>
2024-03-16 17:02:57 -0500

    updates

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,18 +9,22 @@ const HeroSection = () => {
           <h1 className="mb-4 text-4xl font-extrabold text-white lg:text-6xl">
             Hello, I&apos;m Kadyn
           </h1>
-          <p className="text-[#adb7be text-lg lg:text-xl">
+          <p className="text-[#adb7be text-lg mb-6 lg:text-xl">
             I am web developer and a mom ☺️
           </p>
+          <div>
+            <button>Hire Me</button>
+            <button>Download CV</button>
+          </div>
         </div>
-        <div className="col-span-5">
-          <div className="bg-[#181818] w-[500px] h-[500px] relative overflow-hidden rounded-full">
+        <div className="col-span-5 mt-4 place-self-center lg:mt-0">
+          <div className="bg-[#181818] w-[250px] h-[250px] relative overflow-hidden rounded-full">
             <Image
               src="/images/jedikay.jpg"
               alt="jedikay image"
-              className="absolute transform -translate-x-1 -translate-y-1 top-1 left-1"
-              width={500}
-              height={500}
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
             />
           </div>
         </div>
