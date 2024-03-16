03b67e3d97329116db562bf84de32196314f7cfe
Jedikay <kadyns0604@gmail.com>
2024-03-16 14:23:54 -0500

    fix: fixed image shape

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,28 +1,32 @@
 import React from "react";
-import Image from "next/image"
+import Image from "next/image";
 
 const HeroSection = () => {
-    return (
-        <section>
-            <div className="grid grid-cols-1 lg:grid-cols-12">
-                <div className="col-span-7 place-self-center">
-            <h1 className="mb-4 text-4xl font-extrabold text-white lg:text-6xl">Hello, I&apos;m Kadyn</h1>
-            <p className="text-[#adb7be text-lg lg:text-xl">I am a web developer.</p>
-            </div>
-            <div className="col-span-5 overflow-hidden rounded-full">
-            <div className="bg-[#181818] w-[500px] h-[500px] relative">
+  return (
+    <section>
+      <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className="col-span-7 place-self-center">
+          <h1 className="mb-4 text-4xl font-extrabold text-white lg:text-6xl">
+            Hello, I&apos;m Karyn
+          </h1>
+          <p className="text-[#adb7be text-lg lg:text-xl">
+            I am very good web developer
+          </p>
+        </div>
+        <div className="col-span-5">
+          <div className="bg-[#181818] w-[500px] h-[500px] relative overflow-hidden rounded-full">
             <Image
-            src="/images/jedikay.jpg"
-            alt="jedikay image"
-            className="absolute transform -translate-x-1 -translate-y-1 top-1 left-1"
-            width={500}
-            height={300}
+              src="/images/jedikay.jpg"
+              alt="jedikay image"
+              className="absolute transform -translate-x-1 -translate-y-1 top-1 left-1"
+              width={500}
+              height={500}
             />
-            </div>
-            </div>
-            </div>
-        </section>
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HeroSection;
