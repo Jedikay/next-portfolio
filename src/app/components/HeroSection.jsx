f62d72727b198eb0bdb718d4bd77fe246e25b2d7
Jedikay <kadyns0604@gmail.com>
2024-03-17 01:50:26 -0500

    initial setup

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,20 +1,46 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="mb-4 text-4xl font-extrabold text-white lg:text-6xl">
-            Hello, I&apos;m Kadyn
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 text-center place-self-center sm:text-left">
+          <h1 className="mb-4 text-4xl font-extrabold text-white sm:text-5xl lg:text-6xl">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Kadyn",
+                1000,
+                "a Web Developer",
+                1000,
+                "a Front End Developer",
+                1000,
+                "Self-Taught",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
-          <p className="text-[#adb7be text-lg mb-6 lg:text-xl">
-            I am web developer and a mom ☺️
+          <p className="text-[#adb7be text-base sm:text-lg mb-6 lg:text-xl">
+            I am a web developer and a mom 👩🏻‍💻👩🏻‍🍼
           </p>
           <div>
-            <button>Hire Me</button>
-            <button>Download CV</button>
+            <button className="w-full px-6 py-3 mr-4 text-white rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 sm:w-fit hover:bg-slate-200">
+              Hire Me
+            </button>
+            <button className="w-full px-1 py-1 mt-3 text-white rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 sm:w-fit hover:bg-slate-800">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download Resume
+              </span>
+            </button>
           </div>
         </div>
         <div className="col-span-5 mt-4 place-self-center lg:mt-0">
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="container flex flex-col min-h-screen px-12 py-4 mx-auto bg-black">
-      <HeroSection />
+    <main className="container flex flex-col min-h-screen">
+      <Navbar />
+      <div class="container mt-24 px-12 py-4 mx-auto bg-black">
+        <HeroSection />
+      </div>
     </main>
   );
 }
