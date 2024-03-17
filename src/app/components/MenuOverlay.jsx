4ec3f47db81690866c24fe22614a2ce6c6d0c530
John Larson <jl@buildmidwestern.com>
2024-03-17 08:05:58 -0500

    fix: fixed styles, converted to flexbox, added logo

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -3,14 +3,14 @@ import NavLink from "./NavLink";
 
 const MenuOverlay = ({ links }) => {
   return (
-    <ul className="flex flex-col items-center py-4">
+    <ul className="flex w-full flex-col items-center justify-center py-4">
       {links.map((link, index) => (
-        <li key={index}>
+        <li key={index} className="flex w-full items-center justify-center">
           <NavLink href={link.path} title={link.title} />
         </li>
       ))}
     </ul>
-  );
+  )
 };
 
 export default MenuOverlay;
