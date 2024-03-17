c258dc5c699e06e4e04508c8e8a6ae83e9e29ca3
Jedikay <kadyns0604@gmail.com>
2024-03-17 10:17:15 -0500

    changed titles

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -6,24 +6,24 @@ import { TypeAnimation } from "react-type-animation";
 const HeroSection = () => {
   return (
     <section className="w-full max-w-5xl py-8">
-      <div className="flex w-full flex-col items-center justify-center md:flex-row md:justify-between">
+      <div className="flex flex-col items-center justify-center w-full md:flex-row md:justify-between">
         <div className="w-full text-left md:w-auto md:flex-1">
-          <h1 className="mb-4 w-full text-3xl font-extrabold text-white md:text-4xl lg:text-6xl">
-            <span className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent">
+          <h1 className="w-full mb-4 text-3xl font-extrabold text-white md:text-4xl lg:text-6xl">
+            <span className="text-transparent bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text">
               Hello, I&apos;m
             </span>
             <br />
             <TypeAnimation
               sequence={[
-                'Kadyn',
+                "Kadyn",
                 1000,
-                'a Frontend Developer',
+                "Meta Certified",
                 1000,
-                'a Backend Developer',
+                "a Web Developer",
                 1000,
-                'a Fullstack Developer',
+                "an UI/UX Engineer",
                 1000,
-                'a Web Developer',
+                "Kadyn",
                 5000,
               ]}
               wrapper="span"
@@ -35,10 +35,10 @@ const HeroSection = () => {
             I am a web developer and a mom 👩🏻‍💻👩🏻‍🍼
           </p>
           <div>
-            <button className="mr-4 w-full rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 px-6 py-3 text-white hover:bg-slate-200 md:w-fit">
+            <button className="w-full px-6 py-3 mr-4 text-white rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 md:w-fit">
               Hire Me
             </button>
-            <button className="mt-3 w-full rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 px-1 py-1 text-white hover:bg-slate-800 md:w-fit">
+            <button className="w-full px-1 py-1 mt-3 text-white rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 md:w-fit">
               <span className="block rounded-full bg-[#121212] px-5 py-2 hover:bg-slate-800">
                 Download Resume
               </span>
@@ -50,7 +50,7 @@ const HeroSection = () => {
             <Image
               src="/images/jedikay.jpg"
               alt="jedikay image"
-              className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2"
               width={300}
               height={300}
             />
@@ -58,7 +58,7 @@ const HeroSection = () => {
         </div>
       </div>
     </section>
-  )
+  );
 };
 
 export default HeroSection;
