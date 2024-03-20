a6e9b2eefa834a6f68be2e25432fd412b4bee5b4
Jedikay <kadyns0604@gmail.com>
2024-03-20 12:38:19 -0500

    Removed navbar from page.js.

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -5,8 +5,20 @@ const projectsData = [
   {
     id: 1,
     title: "JavaScript Calculator",
-    description: "Calculator built using JavaScript",
+    description: "Calculator built using JavaScript.",
     image: "/images/projects/calc.png",
+  },
+  {
+    id: 1,
+    title: "JavaScript Mole Game",
+    description: "Feed a mole game built using Javascript.",
+    image: "/images/projects/molegame.png",
+  },
+  {
+    id: 1,
+    title: "React To Do List",
+    description: "To Do list built using React",
+    image: "/images/projects/todo.png",
   },
 ];
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-start min-h-screen px-8 py-24">
-      <Navbar />
       <div class="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
