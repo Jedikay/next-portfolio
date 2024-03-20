1b4ff6f5b9873e20d1cfc7a2f7466e8a9ec0717e
Jedikay <kadyns0604@gmail.com>
2024-03-20 13:50:09 -0500

    project section

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -9,13 +9,13 @@ const projectsData = [
     image: "/images/projects/calc.png",
   },
   {
-    id: 1,
+    id: 2,
     title: "JavaScript Mole Game",
     description: "Feed a mole game built using Javascript.",
     image: "/images/projects/molegame.png",
   },
   {
-    id: 1,
+    id: 3,
     title: "React To Do List",
     description: "To Do list built using React",
     image: "/images/projects/todo.png",
@@ -25,8 +25,19 @@ const projectsData = [
 const ProjectsSection = () => {
   return (
     <>
-      <h2>ProjectsSection</h2>
-      <div></div>
+      <h2 className="mt-4 text-4xl font-bold text-center text-white">
+        My Projects
+      </h2>
+      <div>
+        {projectsData.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+          />
+        ))}
+      </div>
     </>
   );
 };
